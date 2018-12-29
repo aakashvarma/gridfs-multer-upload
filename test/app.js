4151226161e7bc0d12f745117084289e3200694c
aakashvarma <aakashvarma18@gmail.com>
2018-12-30 00:37:47 +0530

    ejs added

--- test/app.js
+++ test/app.js
@@ -3,12 +3,26 @@ let fetch = require('node-fetch');
 
 let app = express();
 
+// Middleware
+
+app.set('view engine', 'ejs');
+
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
 app.get('/test/api', (req, res) => {
     setTimeout(() => {
         res.json({
             type: "GET",
             name: "Varma",
-            file: "test"
+            file: "test",
+            menu: [
+                { name: 'Bloody Mary', drunkness: 3 },
+                { name: 'Martini', drunkness: 5 },
+                { name: 'Scotch', drunkness: 10 }
+            ]
         });
     }, 5000 );
 
@@ -28,7 +42,7 @@ app.get('/test/fetch', (req, res) => {
 
     getData(url).then(function(d){
         console.log(d);
-        res.send(d);
+        res.render('index', d);         // rendered html page here
     })
     .catch(function(err){
         console.log(err);
